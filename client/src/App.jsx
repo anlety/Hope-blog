e26ef8d78f92220487204b7f7b1a8fea9a3b4614
anlety <79236211+anlety@users.noreply.github.com>
2024-02-09 17:21:39 +1100

    create a header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
-function App() {
+
+export default function App() {
   return (
-    <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
