5defe8f73837b5510dd1b56ad7854cb70643fc38
anlety <79236211+anlety@users.noreply.github.com>
2024-02-13 05:05:04 +1100

    complete profile functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,12 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
+
 
 
 export default function App() {
@@ -18,6 +21,9 @@ export default function App() {
       <Route path="/projects" element={<Projects />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route element={<PrivateRoute />} >
+      <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
     <Footer />
     </BrowserRouter>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import DashSidebar from "../components/DashSidebar"
+import DashProfile from "../components/DashProfile"
 
 
 export default function Dashboard() {
+  const [tab, setTab] = useState('')
+  const location = useLocation()
+
+  useEffect(()=> {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+  }, [location.search])
   return (
-    <div>Dashboard</div>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* Sidebar */}
+        <div className="w-full md:w-56">
+          <DashSidebar />
+        </div>
+
+        {/* Main */}
+        {tab === 'profile' && <DashProfile />}
+    </div>
   )
 }
